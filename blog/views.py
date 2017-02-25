cac8df079169bea322a9ff6eae577e87d56ace85
Agnes <agnes.p.chu@gmail.com>
2017-02-25 23:10:39 +0000

    add user not working

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, request, redirect, url_for
-
+from flask import render_template, request, redirect, url_for, flash
+from flask.ext.login import login_user
+from werkzeug.security import check_password_hash
 from . import app
-from .database import session, Entry
+from .database import session, Entry, User
 
 PAGINATE_BY = 10
 
@@ -79,3 +80,19 @@ def delete_entry_post(id):
     session.delete(entry)
     session.commit()
     return redirect(url_for("entries"))
+
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("entries"))
